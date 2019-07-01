d6c420087b284b9cfd5a70fc1a172a8ed48eeff2
Marcus Trautwig <marcus.trautwig@1und1.de>
2019-07-01 14:09:39 +0200

    automatically generate self-signed SSL certificate

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 const express = require('express');
 const fs = require('fs');
+const openssl = require('openssl');
 
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
-  
-const app = express();
-const http = require('https').createServer(options, app);
-const io = require('socket.io')(http);
+(async () => {
+    if (!fs.existsSync('key.pem')) {
+        await createPrivateKey();
+    }
+    if (!fs.existsSync('cert.pem')) {
+        await createCertificate();
+    }
+    startServer();
+})();
 
-app.use(express.static('static'));
+function createPrivateKey() {
+    return new Promise((resolve) => {
+        openssl('genrsa', {'out': 'key.pem'}, (exit, stdout, stderr) => {
+            resolve();
+        });    
+    });
+}
 
-io.on('connection', function(socket) {
-    console.log('a user connected');
-    sendPeers(socket);
-    socket.on('disconnect', function() {
+function createCertificate() {
+    return new Promise((resolve) => {
+        let host = 'localhost';
+        openssl('req', {'out': 'cert.pem', 'key': 'key.pem', 'subj': `/CN=${host}/`, 'days': '+365'}, ['-new', '-x509'], (exit, stdout, stderr) => {
+            resolve();
+        });    
+    });
+}
+
+function startServer() {
+    const options = {
+        key: fs.readFileSync('key.pem'),
+        cert: fs.readFileSync('cert.pem')
+    };
+    
+    const app = express();
+    const http = require('https').createServer(options, app);
+    const io = require('socket.io')(http);
+
+    app.use(express.static('static'));
+
+    io.on('connection', function(socket) {
+        console.log('a user connected');
         sendPeers(socket);
+        socket.on('disconnect', function() {
+            sendPeers(socket);
+        });
+        socket.on('view', function(params) {
+            socket.broadcast.emit('view', params);
+        });
+        socket.on('data', function(blob) {
+            socket.broadcast.emit('data', blob);
+        });
     });
-    socket.on('view', function(params) {
-        socket.broadcast.emit('view', params);
-    });
-    socket.on('data', function(blob) {
-        socket.broadcast.emit('data', blob);
-    });
-});
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+    http.listen(3000, function(){
+    console.log('listening on *:3000');
+    });
+}
 
 function sendPeers(socket) {
     io.sockets.clients((err, clients) => {
