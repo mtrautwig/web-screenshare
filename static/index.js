ed608d677a34b50dfd438be5a063090fcb81350f
Marcus Trautwig <marcus@trautwig.de>
2019-07-03 23:07:32 +0200

    add clock, HTML snippets and URL support

--- static/index.js
+++ static/index.js
@@ -42,52 +42,156 @@
         };
     }
 
-    function MediaBroadcast() {
-        var self = this;
-        var socket = new Socket();
+    function MediaDisplay() {
+        var clockTimer = null;
 
-        this.send = function(data) {
-            socket.binary(data);
-        };
+        var clear = function() {
+            if (clockTimer) {
+                window.clearTimeout(clockTimer);
+                clockTimer = null;
+            }
 
-        this.view = function(data) {
             var displayEl = document.getElementById('display');
             while (displayEl.children.length) {
                 displayEl.removeChild(displayEl.children[0]);
             }            
+            return displayEl;
+        };
+
+        var updateClock = function() {
+            //var days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+            var days = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+            var months = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'];
+
+            var clock = document.querySelector('#clock');
+            if (clock) {
+                var now = new Date();
+
+                var clockTime = clock.querySelector('#clock-time');
+                var hr = now.getHours();
+                var min = now.getMinutes();        
+                clockTime.textContent = "" + Math.floor(hr / 10) + Math.floor(hr % 10) + ":" + Math.floor(min / 10) + Math.floor(min % 10);
+
+                var clockDate = clock.querySelector('#clock-date');
+                var date = now.getDate();
+                clockDate.textContent = days[now.getDay()] + ", " + Math.floor(date / 10) + Math.floor(date % 10) + ". " + 
+                    months[now.getMonth()] + " " + now.getFullYear();
+            }
+        };
+
+        this.clear = function() {
+            clear();
+        }
+
+        this.viewClock = function() {
+            var displayEl = clear();
+            var tpl = document.querySelector('#clock-template');
+            var clockEl = document.importNode(tpl.content, true);
+            displayEl.appendChild(clockEl);
+
+            updateClock();
+            clockTimer = window.setInterval(updateClock, 10000);
+        };
+
+        this.viewImage = function(data) {
+            var displayEl = clear();
             var imgEl = document.createElement('img');
             imgEl.src = window.URL.createObjectURL(new Blob([data]));
+            imgEl.onload = (ev) => {
+                window.URL.revokeObjectURL(ev.target.src);
+            };
             displayEl.appendChild(imgEl);
+        };
+
+        this.viewHtml = function(data) {
+            var displayEl = clear();
+            var iframeEl = document.createElement('iframe');
+            iframeEl.srcdoc = '<html><body>' + data + '</body></html>';
+            displayEl.appendChild(iframeEl);
+        };
+
+        this.viewUrl = function(url) {
+            var displayEl = clear();
+            var iframeEl = document.createElement('iframe');
+            iframeEl.src = url;
+            displayEl.appendChild(iframeEl);
+        };
+
+        this.showConnected = function(count) {
+            document.querySelector('#num-screens').textContent = count;
+        };
+    }
+
+    function MediaBroadcast(display) {
+        var socket = new Socket();
+
+        this.clear = function() {
+            display.clear();
+            socket.emit('clear', {});
+        };
+
+        this.viewClock = function() {
+            display.viewClock();
+            socket.emit('clock', {});
+        };
+
+        this.viewImage = function(data) {
+            display.viewImage(data);
+            socket.binary(data);
+        };
+
+        this.viewHtml = function(data) {
+            display.viewHtml(data);
+            socket.emit('html', data);
+        };
+
+        this.viewUrl = function(url) {
+            display.viewUrl(url);
+            socket.emit('url', url);
         };
 
         this.listen = function() {
             socket.on('hello', (params) => {
                 console.log('Peers connected:', params);
+                display.showConnected(params);
             });
             socket.on('binary', (data) => {
-                self.view(data);
+                display.viewImage(data);
+            });
+            socket.on('clock', () => {
+                display.viewClock();
+            });
+            socket.on('html', (data) => {
+                display.viewHtml(data);
+            });
+            socket.on('url', (url) => {
+                display.viewUrl(url);
             });
         };
     }
 
     console.log('script started');
 
-    var screen = new MediaBroadcast();
-    screen.listen();
+    var screen = new MediaDisplay();
+    screen.viewClock();
+
+    var broadcast = new MediaBroadcast(screen);
+    broadcast.listen();
     
-    var overlay = document.querySelector('#overlay');
-    overlay.addEventListener('dragover', (event) => {
+    var toolbar = document.querySelector('#toolbar');
+    document.addEventListener('dragover', (event) => {
         event.preventDefault();
         event.dataTransfer.dropEffect = 'copy';
-        event.target.classList.add('dragging');
+        toolbar.classList.add('dragging');
     });
-    overlay.addEventListener('dragleave', (event) => {
-        event.target.classList.remove('dragging');
-    });
-    overlay.addEventListener('drop', (event) => {
-        event.target.classList.remove('dragging');
-
+    document.addEventListener('dragleave', (event) => {
         event.preventDefault();
+        toolbar.classList.remove('dragging');
+    });
+    document.addEventListener('drop', (event) => {
+        event.preventDefault();
+        toolbar.classList.remove('dragging');
+
         var files = event.dataTransfer.files;
         if (files.length) {
             acceptFile(files[0]);
@@ -98,18 +202,35 @@
         var files = event.clipboardData.files;
         if (files.length) {
             acceptFile(files[0]);
+        } else {
+            var text = event.clipboardData.getData('text/plain');
+            if (text && (text.indexOf('http:') == 0 || text.indexOf('https:') == 0)) {
+                broadcast.viewUrl(text);
+            }
         }
     });
 
     function acceptFile(file) {
         var reader = new FileReader();
-        reader.onloadend = (ev) => {
-            var data = ev.target.result;
-            screen.view(data);
-            screen.send(data);
-        };
-        reader.readAsArrayBuffer(file);
+        if (file.type.indexOf('image/') == 0) {
+            reader.onloadend = (ev) => {
+                var data = ev.target.result;
+                broadcast.viewImage(data);
+            };
+            reader.readAsArrayBuffer(file);
+        } else if ("text/html" == file.type) {
+            reader.onloadend = (ev) => {
+                var data = ev.target.result;
+                broadcast.viewHtml(data);
+            };
+            reader.readAsText(file);            
+        } else {
+            console.log('Unsupported file:', file);
+        }
     }
-
+    
+    document.querySelector('#btn-clock').addEventListener('click', () => {
+       broadcast.viewClock();
+    });
 })();
 
